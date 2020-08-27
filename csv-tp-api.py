bc00c8b723fe62a069357164b6675230e8e68be6
Ryan Haber <ryan.haber@gmail.com>
2020-08-27 15:40:50 -0400

    begins JSON assembly

--- csv-tp-api.py
+++ csv-tp-api.py
@@ -2,12 +2,19 @@ import csv
 import json
 import requests
 
-a = 1
 # read CSV
 # River will write code to do this
 
 # assemble JSON
 # this is the flow chart
+
+headerObject = {}
+
+for row in csvFile:
+	record = headerObject
+	for field in record:
+		XYZ
+
 
 # make API request
 # Ryan will write code to do this
