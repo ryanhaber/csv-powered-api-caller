3d48fb80aa09a86a8ec25f0bffd16a354368ac55
Ryan Haber <ryan.haber@gmail.com>
2020-08-27 14:47:04 -0400

    adds python file and first test data

---
+++ csv-tp-api.py
@@ -0,0 +1,17 @@
+import csv
+import json
+import requests
+
+
+# read CSV
+# River will write code to do this
+
+# assemble JSON
+# this is the flow chart
+
+# make API request
+# Ryan will write code to do this
+
+# validate response
+
+# issue any alerts necessary
