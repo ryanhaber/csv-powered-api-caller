eef0d2355b3522fd0aed418e699843d43ee6c4d4
Ryan Haber <ryan.haber@gmail.com>
2020-08-28 13:29:26 -0400

    adds stub for API call

--- csv-tp-api.py
+++ csv-tp-api.py
@@ -20,6 +20,12 @@ for row in csvFile:
 # make API request
 # Ryan will write code to do this
 
+url = 'https://maryland.formability.enovationallabs.com/api/v2/object_models/5f10a8b40c6fd700fe4baebb/records?='
+urlParams = {'key1': 'value1', 'key2': 'value2'}
+body = {'key':'value'}
+
+requestResult = requests.post(url, params = urlParams, data = body)
+
 # validate response
 
 # issue any alerts necessary
