68442c2018c48f5ce217de99f932210e9564bae9
Ryan Haber <ryan.haber@gmail.com>
2020-10-12 16:16:45 -0400

    cleans up file, adds comments

--- experiment-with-pandas/playwithpandas.py
+++ experiment-with-pandas/playwithpandas.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import json
 
-d = {'name': ['ryan', 'john', 'mary'], 'age':[43,32,21]}
+# load the CSV, convert it to json, then build a python dictionary from the json
 df = pd.read_csv('scripttestdata1.csv')
-
-print(df)
-
-convertToJson = df.to_json(orient='records')
-
+convertToJson = df.to_json(orient='records')  #build the json record-wise rather than column-wise
 convertToDict = json.loads(convertToJson)
 
 
+# go through each record and replace one value with the object {'value': XYZ} just like Formability's API expects
 for record in convertToDict:
 	value = record['TEMAIL']
 	record['TEMAIL'] = {'value' : value}
 
+# convert back to json string for saving to a file
 convertToJson = json.dumps(convertToDict)
 
+# save to file
 outfile = open('scripttestdata1.json', 'w')
 outfile.write(convertToJson)
 outfile.close()
