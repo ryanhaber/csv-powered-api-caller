71eabe0c4177fec5d26481a9b716e4e03d6813c1
Ryan Haber <ryan.haber@gmail.com>
2020-10-13 10:03:36 -0400

    cleans up, adds cl args for input and output files

--- experiment-with-pandas/migrate.py
+++ experiment-with-pandas/migrate.py
@@ -2,25 +2,24 @@ import pandas as pd
 import json
 import sys
 
-# get input and output files from commandline
 inputfile = sys.argv[1]
 outputfile = sys.argv[2]
 
 # load the CSV, convert it to json, then build a python dictionary from the json
-dataframe = pd.read_csv(inputfile)
-dataInJson = dataframe.to_json(orient='records')  #build the json record-wise rather than column-wise
-dataAsDict = json.loads(dataInJson)
+df = pd.read_csv(inputfile)
+convertToJson = df.to_json(orient='records')  #build the json record-wise rather than column-wise
+convertToDict = json.loads(convertToJson)
 
 
 # go through each record and replace one value with the object {'value': XYZ} just like Formability's API expects
-for record in dataAsDict:
+for record in convertToDict:
 	value = record['TEMAIL']
 	record['TEMAIL'] = {'value' : value}
 
 # convert back to json string for saving to a file
-dataInJson = json.dumps(dataAsDict)
+convertToJson = json.dumps(convertToDict)
 
 # save to file
 outfile = open(outputfile, 'w')
-outfile.write(dataInJson)
+outfile.write(convertToJson)
 outfile.close()
