272bfb31f2da384535a0ced6abe87cec1d26b5eb
Ryan Haber <ryan.haber@gmail.com>
2020-12-10 15:54:16 -0500

    renames and tweaks

--- convert-cvs-to-formarecord.py
+++ convert-cvs-to-formarecord.py
@@ -9,6 +9,8 @@ outputfile = sys.argv[2]
 
 convertToDict = csv_to_record.csv_to_record(inputfile)
 
+print("There are ", len(convertToDict), " records.")
+
 convertToJson = json.dumps(convertToDict)
 
 # save to file
