c113398eb6f9b0066eb22f7a6e179c9ce50e262b
Ryan Haber <ryan.haber@gmail.com>
2020-12-10 15:27:01 -0500

    modularizes the function to convert csv files to json files of formability records

---
+++ convert-from-cvs-to-json.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import json
+import sys
+from lib import csv_to_record
+
+inputfile = sys.argv[1]
+outputfile = sys.argv[2]
+
+
+convertToDict = csv_to_record.csv_to_record(inputfile)
+
+convertToJson = json.dumps(convertToDict)
+
+# save to file
+outfile = open(outputfile, 'w')
+outfile.write(convertToJson)
+outfile.close()
---
+++ lib/csv_to_record.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import json
+import sys
+
+# inputfile is a filename as a string
+
+def csv_to_record(inputfile):
+
+	# load the CSV, convert it to json, then build a python dictionary from the json
+	df = pd.read_csv(inputfile)
+	convertToJson = df.to_json(orient='records')  #build the json record-wise rather than column-wise
+	convertToDict = json.loads(convertToJson)
+
+
+	# go through each record and replace each value with the object {'value': XYZ} just like Formability's API expects
+	for record in convertToDict:
+		for column in record:	
+			value = record[column]
+			record[column] = {'value' : value}
+
+	# convert back to json string for saving to a file
+	convertToJson = json.dumps(convertToDict)
+
+	return convertToDict
